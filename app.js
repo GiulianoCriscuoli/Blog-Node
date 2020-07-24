0b996cb4281b3894659371da1c8698014730dd42
Giuliano Criscuoli <giulianodias450@gmail.com>
2020-07-23 21:49:51 -0300

    Habilitando a pasta public

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended:true }));
 
+app.use(express.static(__dirname + '/public'));
+
 // configurando mensagens flash
 
 app.use(cookieParser(process.env.SECRET));
@@ -37,6 +39,8 @@ app.use((req, res, next) => {
     next();
 
 });
+
+
 
 // configuração das rotas
 
